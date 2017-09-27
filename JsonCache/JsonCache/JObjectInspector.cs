1e87e6ed7d3814de0b63302e43f1234405c7fe5f
jamir Araújo <jamir.c.a@hotmail.com>
2017-09-27 08:27:24 -0300

    ObjectInspector

---
+++ JsonCache/JsonCache/JObjectInspector.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonCache
+{
+    public class JObjectInspector : IObjectInspector<JObject>
+    {
+        public void InspectObject(JObject value)
+        {
+        }
+    }
+}
