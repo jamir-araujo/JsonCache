6b2bc99c8e8efe157a7133380786c0625b0582f4
Jamir Castro Araújo <jamir.c.a@hotmail.com>
2017-10-12 21:30:40 -0300

    Implementing MapedRuntimeInspector

---
+++ JsonCache/GraphCache.Runtime/MapedRuntimeInspector.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace GraphCache.Runtime
+{
+    public class MapedRuntimeInspector : IInspector<object>
+    {
+        private readonly ITypeMapFactory _typeMapFactory;
+
+        public MapedRuntimeInspector(ITypeMapFactory typeMapFactory)
+        {
+            _typeMapFactory = typeMapFactory;
+        }
+
+        public void Inspect(object value, Found<object> found, DependencyFound<object> dependencyFound)
+        {
+            InspectInternal(value, found, dependencyFound, new HashSet<object>(), null);
+        }
+
+        private void InspectInternal(
+            object value,
+            Found<object> found,
+            DependencyFound<object> dependencyFound,
+            ISet<object> workingItems,
+            Dependency<object> dependency)
+        {
+            if (value == null && !workingItems.Add(value))
+            {
+                return;
+            }
+
+            if (value is IList list)
+            {
+                for (int index = 0; index < list.Count; index++)
+                {
+                    InspectInternal(list[index], found, dependencyFound, workingItems, dependency);
+                }
+            }
+            else
+            {
+                var key = found(value);
+
+                if (value != null)
+                {
+                    dependency = null;
+                }
+
+                InspectProperties(value, found, dependencyFound, workingItems, dependency, key);
+            }
+        }
+
+        private void InspectProperties(
+            object value,
+            Found<object> found,
+            DependencyFound<object> dependencyFound,
+            ISet<object> workingItems,
+            Dependency<object> dependency,
+            string key)
+        {
+            var typemap = _typeMapFactory.Create(value.GetType());
+
+            for (int index = 0; index < typemap.Properties.Count; index++)
+            {
+                var property = typemap.Properties[index];
+
+                var propertyAccessor = property.PropertyAccessor;
+                var propertyValue = propertyAccessor.GetValue(value);
+
+                if (propertyValue is IList list)
+                {
+                    InspectListProperty(list, propertyAccessor, found, dependencyFound, workingItems, dependency, key);
+                }
+                else
+                {
+                    InspectProperty(propertyValue, propertyAccessor, found, dependencyFound, workingItems, dependency, key);
+                }
+            }
+        }
+
+        private void InspectListProperty(
+            IList propertyValue,
+            PropertyAccessor accessor,
+            Found<object> found,
+            DependencyFound<object> dependencyFound,
+            ISet<object> workingItems,
+            Dependency<object> dependency,
+            string key)
+        {
+            for (int index = 0; index < propertyValue.Count; index++)
+            {
+                var value = propertyValue[index];
+                var dependencyForIndex = dependency;
+
+                if (dependencyForIndex == null)
+                {
+                    if (key != null)
+                    {
+                        dependencyForIndex = new DirectIndexedDependency(accessor, index, key);
+                    }
+                }
+                else
+                {
+                    dependencyForIndex = new ChainedIndexedDependency(accessor, index, dependencyForIndex);
+                }
+
+                InspectInternal(value, found, dependencyFound, workingItems, dependencyForIndex);
+
+                if (dependencyForIndex != null)
+                {
+                    dependencyFound(value, dependencyForIndex);
+                }
+            }
+        }
+
+        private void InspectProperty(
+            object propertyValue,
+            PropertyAccessor accessor,
+            Found<object> found,
+            DependencyFound<object> dependencyFound,
+            ISet<object> workingItems,
+            Dependency<object> dependency,
+            string key)
+        {
+            if (dependency == null)
+            {
+                if (key != null)
+                {
+                    dependency = new DirectDependency(accessor, key);
+                }
+            }
+            else
+            {
+                dependency = new ChainedDependency(accessor, dependency);
+            }
+
+            InspectInternal(propertyValue, found, dependencyFound, workingItems, dependency);
+
+            if (dependency != null)
+            {
+                dependencyFound(propertyValue, dependency);
+            }
+        }
+    }
+}
---
+++ JsonCache/GraphCache.Runtime/TypeMap.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace GraphCache.Runtime
+{
+    public interface ITypeMapFactory
+    {
+        TypeMap Create(Type type);
+    }
+
+    public class TypeMapFactory : ITypeMapFactory
+    {
+        private readonly ConcurrentDictionary<Type, TypeMap> _cache;
+        private readonly Func<Type, TypeMap> _createTypeMap;
+        private readonly Func<PropertyInfo, bool> _propertyFilter;
+        private readonly IPropertyAccessorFactory _propertyAccessorFactory;
+
+        public TypeMapFactory(IPropertyAccessorFactory propertyAccessorFactory, Func<PropertyInfo, bool> propertyFilter)
+        {
+            _propertyAccessorFactory = propertyAccessorFactory;
+            _propertyFilter = propertyFilter;
+
+            _cache = new ConcurrentDictionary<Type, TypeMap>();
+            _createTypeMap = CreateTypeMap;
+        }
+
+        public TypeMap Create(Type type)
+        {
+            return _cache.GetOrAdd(type, _createTypeMap);
+        }
+
+        private TypeMap CreateTypeMap(Type type)
+        {
+            var propertyList = new List<Property>();
+            var properties = type.GetTypeInfo().GetProperties();
+            for (int index = 0; index < properties.Length; index++)
+            {
+                var property = properties[index];
+                if (DefaultPropertyFilter(property) && _propertyFilter(property))
+                {
+                    propertyList.Add(CreateProperty(property));
+                }
+            }
+
+            return new TypeMap(type, propertyList);
+        }
+
+        private Property CreateProperty(PropertyInfo property)
+        {
+            return new Property(property, _propertyAccessorFactory.Create(property));
+        }
+
+        private static bool DefaultPropertyFilter(PropertyInfo propertyInfo)
+        {
+            if (propertyInfo.PropertyType.IsPrimitive)
+            {
+                return false;
+            }
+
+            if (propertyInfo.CanRead && propertyInfo.CanWrite)
+            {
+                return false;
+            }
+
+            if (propertyInfo.PropertyType == typeof(string))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    public class TypeMap
+    {
+        public Type Type { get; }
+
+        public IReadOnlyList<Property> Properties { get; }
+
+        public TypeMap(Type type, List<Property> properties)
+        {
+            Type = type;
+            Properties = properties;
+        }
+    }
+
+    public class Property
+    {
+        public PropertyInfo PropertyInfo { get; }
+        public PropertyAccessor PropertyAccessor { get; }
+
+        public Property(PropertyInfo propertyInfo, PropertyAccessor propertyAccessor)
+        {
+            PropertyInfo = propertyInfo;
+            PropertyAccessor = propertyAccessor;
+        }
+    }
+}
